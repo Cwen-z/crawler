5b6bef5e090fd57bda3bca69255081736c49a5cc
cwen <652674530@qq.com>
2017-10-29 22:48:21 +0800

    urllib2_re_xpath_bs

---
+++ moviedouban.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from selenium import webdriver
+import time
+
+driver = webdriver.PhantomJS()
+driver.get("https://movie.douban.com/typerank?type_name=剧情&type=11&interval_id=100:90&action=")
+
+time.sleep(3)
+# 向下滚动10000像素
+js = "document.body.scrollTop=10000"
+driver.save_screenshot("douban.png")
+# 执行JS语句
+driver.execute_script(js)
+time.sleep(10)
+driver.save_screenshot("newdouban.png")
+driver.quit()
